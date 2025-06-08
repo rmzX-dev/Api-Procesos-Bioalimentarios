dfe92d0d73c37f7772b9d4ed5e7ff767a8bdc98b
Kevin11ts <20233l001135@utcv.edu.mx>
2025-06-08 04:29:08 -0600

    style: Cambio en el nombre de las variables

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT || 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-module.exports = pool;
+export default pool;
