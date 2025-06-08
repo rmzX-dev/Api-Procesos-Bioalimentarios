aac89e5fde788bca450255d896e34a18c09ee307
Kevin11ts <20233l001135@utcv.edu.mx>
2025-06-08 04:30:10 -0600

    fix: Cambio a modelo ES

--- config/swagger.js
+++ config/swagger.js
@@ -1,31 +1,31 @@
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-require('dotenv').config();
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'INTEGRATIVE PROJECT API.',
-            version: '1.0.0', 
-            description: 'Api Procesos BioAlimentarios',
-        },
-        servers: [
-            {
-                url: 'http://localhost:'+process.env.PORT ,
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "INTEGRATIVE PROJECT API.",
+      version: "1.0.0",
+      description: "Api Procesos BioAlimentarios",
     },
-    apis: ['./app/routes/*.js'],
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 3000}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Aquí van las rutas donde están los comentarios Swagger
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 
 const swaggerDocs = (app) => {
-    app.use('/api-docs', swaggerUi.serve,
-        swaggerUi.setup(swaggerSpec));
-        console.log('Swagger docs available at http://localhost:'+
-            process.env.PORT+'/api-docs');
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  console.log(`Swagger docs disponibles en http://localhost:${process.env.PORT || 3000}/api-docs`);
 };
 
-module.exports = swaggerDocs;
+export default swaggerDocs;
