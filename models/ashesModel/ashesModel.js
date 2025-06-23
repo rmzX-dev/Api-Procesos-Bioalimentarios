a813086319fb29eb242fc5e9b3b50a1a4ca55986
xSE7EN06 <agao6105@gmail.com>
2025-06-22 18:44:19 -0600

    Modificación en modelos de cenizas y carbohidratos

--- models/ashesModel/ashesModel.js
+++ models/ashesModel/ashesModel.js
@@ -1,4 +1,4 @@
-import pool from "../../config/db";
+import pool from "../../config/db.js";
 
 class Ashes{
     static async createAnalisisAshes(data){
--- models/carbohydratesModel/carbohydratesModel.js
+++ models/carbohydratesModel/carbohydratesModel.js
@@ -1,4 +1,4 @@
-import pool from "../../config/db"; 
+import pool from "../../config/db.js"; 
 
 class Carbohydrates {
     static async createAnalisisCarbs(data) {
