4e2239fa1e6107d660ef7edef5a3cb24fdecb2de
xSE7EN06 <agao6105@gmail.com>
2025-07-15 19:34:16 -0600

    refactor(moistureModel): se eliminaron datos no deseados en la inserción

--- models/moistureModel/moistureModel.js
+++ models/moistureModel/moistureModel.js
@@ -1,13 +1,13 @@
 import pool from "../../config/db.js";
 
 class Moisture {
-    static async createAnalisisHumedad(data) {
-        const { idAnalisis, resultado, unidad, metodoReferencia, acreditacion } = data;
+    static async createMoisture(data) {
+        const {idAnalisis, resultado, acreditacion } = data;
 
         const result = await pool.query(
-            `INSERT INTO analisishumedad (idAnalisis, resultado, unidad, metodoReferencia, acreditacion)
-             VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-            [idAnalisis, resultado, unidad, metodoReferencia, acreditacion]
+            `INSERT INTO analisishumedad (idAnalisis, resultado, acreditacion)
+             VALUES ($1, $2, $3) RETURNING *`,
+            [idAnalisis, resultado, acreditacion]
         );
 
         return result.rows[0];
@@ -19,13 +19,6 @@ class Moisture {
             [idAnalisis]
         );
         return result.rows[0];
-    }
-
-    static async getAllMoisture(){
-        const result = await pool.query(
-            'SELECT * FROM analisishumedad'
-        );
-        return result.rows;
     }
 }
 
